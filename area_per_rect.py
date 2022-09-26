9160168414426b4870d26d2cd5041479ce59db62
Lloyd Najac <lloyd.najac@stu.ocsb.ca>
2022-09-26 20:52:48 +0000

    created area_per_rect.py and basic_math.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Lloyd Najac
+# Date: Sep. 26, 2022
+# This program calculates the area & perimeter of a 6cm X 3cm rectangle.
+
+
+def main():
+    print("For a rectangle with lenght 6cm and width 3 cm")
+    print()
+    print("The area is: {}cm^2".format(6 * 3))
+    print("The perimeter is {}cm".format(2 * (6 + 3)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Lloyd Najac
+# Date: Sep. 26, 2022
+# This program does basic math.
+
+
+def main():
+    print("5+4-1={}".format(5 + 4 - 1))
+    print("6*3+{}".format(6 * 3))
+
+
+if __name__ == "__main__":
+    main()
